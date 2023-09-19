4f0210cb6fa2ba822a4b1cfddb76a41e644a7dc2
ROTHIRSCH\kimin <0923katomegumi@gmail.com>
2023-09-19 16:40:35 +0900

    Updated files

--- arcalive.py
+++ arcalive.py
@@ -1,9 +1,7 @@
-import json
 import requests
 import datetime
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from dateutil.relativedelta import relativedelta
 
 base_url = 'https://arca.live'
 url = 'https://arca.live/b/gaijin'
@@ -11,74 +9,38 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
 }
 
-current_date = datetime.datetime.now()
 
-for _page in range(3):
-    _url = url + '?p=' + str(_page)
-    _response = requests.get(url = _url, headers = headers)
-    _soup = BeautifulSoup(_response.text, 'html.parser')
+def get_Page(pages, date):
+    for page in range(pages):
+        _url = url + '?p=' + str(page)
+        response = requests.get(url = _url, headers = headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    selected_date = current_date + relativedelta(days = -1)
+        # 글 제목을 추출
+        for tag in soup.select('span.vcol.col-title > span.title'):
+            _title = tag.text
+            return _title
 
-    for tag in _soup.select('span.vcol.col-title > span.title'):
-        _title = tag.text
-        print(_title)
+        # 글 링크를 추출
+        for href in soup.select('a.vrow.column'):
+            _url = href.attrs.get('href')
 
-    for href in _soup.select('a.vrow.column'):
-        _url = href.attrs.get('href')
+            if _url is None:
+                continue
+            else:
+                return base_url + _url
 
-        if _url is None:
-            continue
-        else:
-            print(f'{base_url + _url}')
+        # 글 작성일을 추출
+        for _element in soup.select('span.vcol.col-time'):
+            if _element.text == '작성일':
+                continue
+            _time_element = _element.select_one("time").text
+            return _time_element
 
-    i = 0
-    for _el in _soup.select('span.vcol.col-time'):
-        if i == 0:
-            i += 1
-            continue
-        _time_el = _el.select("time")
-        print(_time_el[0]["datetime"])
 
-"""boards = []
-page = 1
-loop = True
-current_date = datetime.datetime.now()
+def input_Pages():
+    _input = input("Type in the number pages you want to scrap: ")
+    return int(_input)
 
-while loop:
-    _url = url + "?p=" + str(page)
-    # print(_url)
-    res = requests.get(url = _url, headers = headers)
-    soup = BeautifulSoup(res.text, 'html.parser')
 
-    posts = soup.select(".list-table table")
-    past_date = current_date + relativedelta(minutes = -30)
-
-    for post in posts:
-        post_title = post.select_one(".title").text
-        post_url = url + post.select_one("a").attrs.get("href")
-        post_date = datetime.datetime.strptime(post.select_one(".date").text, "%Y. %m. %d.")
-
-        if post_date < past_date:   # 현재 시점보다 1개월 더 지난 게시글이면 종료
-            loop = False
-            # print(page, str(post_date), post_title)
-            break
-
-        board = {
-            "title": post_title,
-            "url": post_url,
-            "date": str(post_date)
-        }
-        boards.append(board)
-
-    page = page + 1
-
-data = {
-    "past_date": str(past_date),
-    "boards": boards
-}
-
-json_date = json.dumps(data)
-
-with open("./past_date_boards.json", "w") as f:
-    f.write(json_date)"""
+get_Page(input_Pages())
