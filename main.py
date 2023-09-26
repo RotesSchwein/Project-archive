88a3c398ea4530a185c718eab3468207c27e8624
ROTHIRSCH\kimin <0923katomegumi@gmail.com>
2023-09-26 13:59:33 +0900

    Updated file for code review

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from selenium_scrapper import SeleniumScrapper
 
 
 if __name__ == '__main__':
-    print(BasicScrapper.get_page_info())
+    # print(BasicScrapper.get_page_info())
+    SeleniumScrapper.click_to_next_page()
